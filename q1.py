89d983d94682d5942253f1cf0d196ad67b1e36fc
Chirag Shilwant <48115585+Chirag-Shilwant@users.noreply.github.com>
2020-11-06 11:18:40 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -5,17 +5,21 @@ dfs1 =[]
 root = None
 E1 = None
 E2 = None
-
+empCurr = None
+rootFound = False
+lcaOfPairs = []
 def lca(emp1, emp2):
  if(emp1 == emp2):
-  print(emp1)
-  print(E1, " is ",int(employeeTree[E1][1])-int(employeeTree[emp1][0])," below ",emp1)
-  print(E2, " is ",int(employeeTree[E2][1])-int(employeeTree[emp1][0])," below ", emp1)
-  return
+  #print(emp1)
+  #print(E1, " is ",int(employeeTree[E1][1])-int(employeeTree[emp1][0])," below ",emp1)
+  #print(E2, " is ",int(employeeTree[E2][1])-int(employeeTree[emp1][0])," below ", emp1)
+  if(emp1 == root):
+   rootFound = True
+   #print(rootFound)
+  return emp1
  else:
-  lca(employeeTree[emp1][0], employeeTree[emp2][0])	  
-   
-   
+  return lca(employeeTree[emp1][0], employeeTree[emp2][0])	  
+      
 f = open("org.json","r") 
 data = json.load(f) 
 
@@ -32,47 +36,74 @@ for i in range(0, len(data)):
 #print(employeeTree)
 
 
-emp1 = input("")  
-emp2 = input("")
-
-E1 = emp1
-E2 = emp2
-print("\n")
-if(emp1 == root or emp2 == root):
- print("No LCA")
-elif(emp1 == emp2):
- print(employeeTree[emp1][0])
- print(E1, " is 1 level below ",employeeTree[emp1][0])
- print(E2, " is 1 level below ",employeeTree[emp2][0]) 
-else:
- e1List = employeeTree[emp1]
- el1 = e1List[1]
- e2List = employeeTree[emp2]
- el2 = e2List[1]
- 
- if(el1 == el2):
-  lca(emp1, emp2)
- elif(el1 > el2):
-  while(el1!=el2):
-   emp1 = employeeTree[emp1][0]
-   el1-=1
-  if(emp1 == emp2):
-   ans = employeeTree[emp1][0]
-   print(ans)
-   print(E1, " is ",int(employeeTree[E1][1])-int(employeeTree[ans][0])," below ",ans)
-   print(E2, " is ",int(employeeTree[E2][1])-int(employeeTree[ans][0])," below ", ans)   
-  else:  
-   lca(emp1, emp2)
+inputList = input("").split()
+#print(inputList)
+#print("\n")
+#emp1 = inputList[1]  
+for e in range(1, len(inputList)-1):
+ #E1 = emp1
+ #E2 = inputList[e]
+ emp1 = inputList[e] 
+ emp2 = inputList[e+1]
+ #print(emp1, emp2)
+ #print("\n")
+ if(emp1 == root or emp2 == root):
+  #print("No LCA")
+  rootFound = True
+  break
+ elif(emp1 == emp2):
+  lcaOfPairs.append(employeeTree[emp1][0])
+  #print()
+  #print(E1, " is 1 level below ",employeeTree[emp1][0])
+  #print(E2, " is 1 level below ",employeeTree[emp2][0]) 
  else:
-  while(el1!=el2):
-   emp2 = employeeTree[emp2][0]
-   el2-=1
-  if(emp1 == emp2):
-   ans = employeeTree[emp1][0]
-   print(ans)
-   print(E1, " is ",int(employeeTree[E1][1])-int(employeeTree[ans][0])," below ",ans)
-   print(E2, " is ",int(employeeTree[E2][1])-int(employeeTree[ans][0])," below ", ans)  
+  e1List = employeeTree[emp1]
+  el1 = e1List[1]
+  e2List = employeeTree[emp2]
+  el2 = e2List[1]
+  #print("\n")
+  #print(el1,el2)
+  if(el1 == el2):
+   temp = lca(emp1, emp2)
+   lcaOfPairs.append(temp)
+   #print(emp1)
+  elif(el1 > el2):
+   while(el1!=el2):
+    emp1 = employeeTree[emp1][0]
+    el1-=1
+   if(emp1 == emp2):
+    lcaOfPairs.append(employeeTree[emp1][0])
+    #print(ans)
+    #print(E1, " is ",int(employeeTree[E1][1])-int(employeeTree[ans][0])," below ",ans)
+    #print(E2, " is ",int(employeeTree[E2][1])-int(employeeTree[ans][0])," below ", ans)   
+   else:  
+    temp = lca(emp1, emp2)
+    lcaOfPairs.append(temp)
   else:
-   lca(emp1, emp2)   
+   while(el1!=el2):
+    emp2 = employeeTree[emp2][0]
+    el2-=1
+   if(emp1 == emp2):
+    lcaOfPairs.append(employeeTree[emp1][0])
+    #print(ans)
+    #print(E1, " is ",int(employeeTree[E1][1])-int(employeeTree[ans][0])," below ",ans)
+    #print(E2, " is ",int(employeeTree[E2][1])-int(employeeTree[ans][0])," below ", ans)  
+   else:
+    temp = lca(emp1, emp2)
+    lcaOfPairs.append(temp) 
 
-f.close() 
+ f.close()
+if(rootFound):
+ print("No LCA")
+ 
+else:
+ level = 999999
+ ansEmp = None 
+ for emp in lcaOfPairs:
+  if(int(employeeTree[emp][0]) < level):
+   level = int(employeeTree[emp][0])
+   ansEmp = emp
+ print(ansEmp)  
+ for e in range(1,len(inputList)):
+  print(ansEmp, " is ",int(employeeTree[inputList[e]][1])-int(employeeTree[ansEmp][0])," level above ",inputList[e])
+  
